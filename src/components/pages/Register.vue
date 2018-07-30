26c4adbb5752cfae6bacd7f6bb2b44b327bff44d
JackWong992 <jackwong992@163.com>
2018-07-30 16:05:32 +0800

    finish 6-1 Register page

---
+++ src/components/pages/Register.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <van-nav-bar
+      title="立即注册"
+      left-text="返回"
+      left-arrow
+      @click-left="goBack"
+    />
+    <div class="register-panel">
+      <van-field
+        v-model="username"
+        required
+        label="用户名"
+        icon="clear"
+        placeholder="请输入用户名"
+        @click-icon="username=''"
+      />
+      <van-field
+        v-model="password"
+        type="password"
+        label="密码"
+        placeholder="请输入密码"
+        required
+      />
+      <!-- register-button area -->
+      <div class="register-button">
+        <van-button type="primary" size="large">主要按钮</van-button>
+      </div>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        username: '',
+        password: ''
+      }
+    },
+    methods: {
+       goBack(){
+        this.$router.go(-1)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .register-panel {
+    width: 96%;
+    border-radius: .4rem;
+    margin: 20px auto;
+    padding-bottom: 5rem;
+  }
+  .register-button {
+    padding-top: 0.625rem;
+  }
+</style>
